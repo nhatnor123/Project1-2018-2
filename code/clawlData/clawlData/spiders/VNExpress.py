11db6d35c758923dca93783351eef57364e57025
Nguyen Luu Nhat <nhatnor123@gmail.com>
2019-03-07 23:52:11 +0700

    Them ham RegEx

--- code/clawlData/clawlData/spiders/VNExpress.py
+++ code/clawlData/clawlData/spiders/VNExpress.py
@@ -1,31 +1,40 @@
 import scrapy
+import re
+
 
 class ClawlVNExpress(scrapy.Spider):
     name = "VNExpress"
 
     def start_requests(self):
-        urls = ['https://vnexpress.net/thoi-su/bo-truong-giao-thong-de-nghi-ai-mat-giay-phep-lai-xe-deu-phai-thi-lai-3890474.html']
+        urls = [
+            'https://vnexpress.net/thoi-su/bo-truong-giao-thong-de-nghi-ai-mat-giay-phep-lai-xe-deu-phai-thi-lai-3890474.html']
 
         for url in urls:
-            yield scrapy.Request(url= url, callback= self.parse_artilce)
+            yield scrapy.Request(url=url, callback=self.parse_artilce)
 
     def parse_artilce(self, response):
         artilce = {}
-        artilce['TITLE'] = response.xpath('/html/body/section[2]/section[1]/section[1]/h1/text()').extract()[0].encode('utf-8').strip()
-        artilce['DESCRIPTION'] = response.xpath('/html/body/section[2]/section[1]/section[1]/p[1]/text()').extract()[0].encode('utf-8').strip()
-        artilce['CONTENT'] = response.xpath('/html/body/section[2]/section[1]/section[1]/article').extract()[0].encode('utf-8').strip()
+        artilce['TITLE'] = response.xpath('/html/body/section[2]/section[1]/section[1]/h1/text()').extract()[0].encode(
+            'utf-8').strip()
+        artilce['DESCRIPTION'] = response.xpath('/html/body/section[2]/section[1]/section[1]/p[1]/text()').extract()[
+            0].encode('utf-8').strip()
+        artilce['CONTENT'] = response.xpath('/html/body/section[2]/section[1]/section[1]/article').extract()[0].encode(
+            'utf-8').strip()
 
         index = 1
 
         for content in response.xpath('/html/body/section[2]/section[1]/section[1]/article/p[@class="Normal"]'):
             artilce[index] = content.extract().encode('utf-8').strip()
-            index +=1
-
+            #artilce[index] = re.sub(re.compile('<.*?>'), "", artilce[index])
+            artilce[index] = re.compile(r'<[^>]+>').sub('', artilce[index])
+            index += 1
 
         for key, text in artilce.iteritems():
             print("\n")
-            if type(key) is int :
-                print("{key} : {text}".format(key= key, text = text))
-            else :
-                print("{key} : {text}".format(key= key.upper(), text = text))
-            print("\n"+type(key)+" : "+type(text))
+            if type(key) is int:
+                print("{key} : {text}".format(key=key, text=text))
+            else:
+                print("{key} : {text}".format(key=key.upper(), text=text))
+            print("")
+            print(type(key))
+            print(type(text))
