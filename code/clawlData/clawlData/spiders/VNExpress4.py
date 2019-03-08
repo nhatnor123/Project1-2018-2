f8bb998548686a2dd6015784066afa43d60f94f8
Nguyen Luu Nhat <nhatnor123@gmail.com>
2019-03-08 19:16:54 +0700

    viet them phan code crawl multi page

--- code/clawlData/clawlData/spiders/VNExpress4.py
+++ code/clawlData/clawlData/spiders/VNExpress4.py
@@ -14,13 +14,14 @@ class VNExpressCollectLink(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.getLink)
 
 
-
+    #get the real link to the page we want crawl data from https://vnexpress.net/thoi-su
     def getLink(self, response):
         for newLink in response.xpath('//*[@class="sidebar_1"]//*[@class="title_news" and not(i)]/a[1]/@href'):
             yield scrapy.Request(url= newLink.extract().encode('utf-8').strip(), callback= self.parse_article )
             #print(newLink.extract().encode('utf-8').strip())
 
 
+    #crawl data from link
     def parse_article(self, response):
         article = {}
         article['TITLE'] = response.xpath('/html/body/section[2]/section[1]/section[1]/h1/text()').extract()[0].encode('utf-8').strip()
